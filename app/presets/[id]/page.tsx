137bcbf78114449d2bcf30e1c593b10aeb967aa7
Aequus Developer <developer@aequus.dev>
2026-09-18 16:59:26 +0100

    Remove unused PRESETS import

--- app/presets/[id]/page.tsx
+++ app/presets/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useParams } from 'next/navigation';
 import Link from 'next/link';
-import { PRESETS, getPreset } from '../../../lib/curvePresets';
+import { getPreset } from '../../../lib/curvePresets';
 import { CurveChart } from '../../../components/CurveChart';
 import { FeeChart } from '../../../components/FeeChart';
 import { LaunchPanel } from '../../../components/LaunchPanel';
